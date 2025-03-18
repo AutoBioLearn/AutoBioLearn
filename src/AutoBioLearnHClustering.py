b02e645a73572c7297cfb5befba6c9de4343cb10
Maria Gabriel <24625280+marialgk@users.noreply.github.com>
2025-03-18 15:06:24 -0300

    Update AutoBioLearnHClustering.py
    Add two visualisation methods

--- src/AutoBioLearnHClustering.py
+++ src/AutoBioLearnHClustering.py
@@ -1,9 +1,100 @@
-# -*- coding: utf-8 -*-
-
+import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
+import seaborn as sns
+import scipy.cluster.hierarchy as sch
 from AutoBioLearnUnsupervisedLearning import AutoBioLearnUnsupervisedLearning
 
 class AutoBioLearnHClustering(AutoBioLearnUnsupervisedLearning):
     
-    def execute_models(self, models:list[str]=[], params={}, section:str=None):
+    def heatmap(self,
+                method:str='',
+                metric:str='',
+                cmap:str='plasma_r',
+                section:str=None,
+                save:bool=True):
+        """
+        method = 'single', 'average', 'complete', 'ward', 'centroid', etc
+        metric = 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 
+                 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 
+                 'jaccard', 'jensenshannon', 'kulczynski1', 'mahalanobis',
+                 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 
+                 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean',
+                 'yule'.
+        """
+        
+        # Get data
+        X = self.data_processor.dataset.get_X(section)
+        try:
+            y = self.data_processor.dataset.get_Y(section)
+        except:
+            y = self.data_processor.dataset.get_Y()
+        
+        # Add colour to the class
+        colours = sns.color_palette("husl", len(y.unique())).as_hex()
+        colours = dict(zip(y.unique(), colours))
+        group = y.replace(colours)
+
+        # Plot heatmap
+        fig = sns.clustermap(X,
+                             row_cluster=False,
+                             method=method,
+                             metric=metric,
+                             z_score=None,
+                             standard_scale=None,
+                             figsize=(8, 12),
+                             row_colors=group,
+                             cmap= cmap)
+        
+        plt.title(f'Dendrogram - {method}', fontsize=16)
+        plt.ylabel(f'{metric}', fontsize=16)
+        
+        # Add legend to class
+        handles = [mpatches.Patch(color=color, label=label) for label, color in colours.items()]
+        plt.legend(handles=handles, bbox_to_anchor=(1.2, 1), loc='upper left')
+        
+        # Save it
+        if save == True:
+            fig.savefig(f'heatmap_{metric}_{method}.png')
             
-        self.data_processor.dataset
+        plt.show()
+        plt.cla()
+    
+    def dendogram(self,
+                  method:str='',
+                  metric:str='',
+                  thresh:int=3,
+                  section:str=None,
+                  save:bool=True):
+        """
+        method = 'single', 'average', 'complete', 'ward', 'centroid', etc
+        metric = 'braycurtis', 'canberra', 'chebyshev', 'cityblock', 
+                 'correlation', 'cosine', 'dice', 'euclidean', 'hamming', 
+                 'jaccard', 'jensenshannon', 'kulczynski1', 'mahalanobis',
+                 'matching', 'minkowski', 'rogerstanimoto', 'russellrao', 
+                 'seuclidean', 'sokalmichener', 'sokalsneath', 'sqeuclidean',
+                 'yule'.
+        """
+        
+        # Get data
+        X = self.data_processor.dataset.get_X(section)
+        
+        # Plot
+        fig, axis = plt.subplots(figsize=(8,12))
+        dend = sch.linkage(X,
+                           method = method,
+                           metric = metric)
+        sch.dendrogram(dend,
+                       labels = X.index,
+                       ax=axis,
+                       orientation='left',
+                       color_threshold=thresh)
+        plt.title(f'Dendrogram - {method}', fontsize=16)
+        plt.ylabel(f'{metric}', fontsize=16)
+        
+        # Save it
+        if save == True:
+            fig.savefig(f'dendogram_{metric}_{method}.png')
+            
+        plt.show()
+        plt.cla()
+        
