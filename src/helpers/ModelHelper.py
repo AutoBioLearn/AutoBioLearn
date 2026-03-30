73707f50cc4b3971787d3e2c86b196e4d649c580
Maria Gabriel <24625280+marialgk@users.noreply.github.com>
2026-03-30 19:55:20 -0300

    Update ModelHelper.py

--- src/helpers/ModelHelper.py
+++ src/helpers/ModelHelper.py
@@ -3,10 +3,11 @@ from lightgbm import LGBMClassifier, LGBMRegressor
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.linear_model import LogisticRegression
 from sklearn.svm import SVC, SVR
-from sklearn.model_selection import KFold, ShuffleSplit, StratifiedKFold,LeaveOneOut, StratifiedShuffleSplit, train_test_split
+from sklearn.model_selection import KFold, ShuffleSplit, StratifiedKFold,LeaveOneOut, StratifiedShuffleSplit
+from sklearn_extra.cluster import KMedoids
 from xgboost import XGBClassifier,XGBRegressor
 
-from sklearn.cluster import KMeans, SpectralClustering, Birch
+from sklearn.cluster import KMeans, SpectralClustering
 from sklearn.mixture import GaussianMixture
 
 
@@ -23,11 +24,7 @@ class ModelHelper(object):
     @staticmethod
     def const_lightboost()-> str: 
         return "lightboost"
-    
-    @staticmethod
-    def const_xgboost()-> str: 
-        return "xgboost"
-    
+
     @staticmethod
     def const_random_forest()-> str: 
         return "random_forest"
@@ -53,8 +50,8 @@ class ModelHelper(object):
         return "spectral"
     
     @staticmethod
-    def const_birch()-> str: 
-        return "birch"
+    def const_pam()-> str: 
+        return "kmedoids"
     
     @staticmethod  
     def get_model(model, model_type="classifier"):
@@ -81,7 +78,7 @@ class ModelHelper(object):
                         ModelHelper.const_kmeans():      KMeans,
                         ModelHelper.const_gaussianmix(): GaussianMixture,
                         ModelHelper.const_spectral():    SpectralClustering,
-                        ModelHelper.const_birch():       Birch,
+                        ModelHelper.const_pam():         KMedoids,
                     }
         
         if model_type.lower() == 'classifier':
